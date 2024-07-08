91f73ab5ab15eb5d4787c51b1c8fa85a7e45bd90
jcampbell57 <jcampbell57@gmail.com>
2024-07-08 00:22:06 -0700

    create, style resume preview component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,54 @@
+import { useState } from 'react'
+import exampleData from '../assets/exampleData.js'
 import ResumeForm from './ResumeForm.jsx'
 import ResumePreview from './ResumePreview.jsx'
 
 function Content() {
+  const [name, setName] = useState(exampleData.general.name)
+  const [email, setEmail] = useState(exampleData.general.email)
+  const [phone, setPhone] = useState(exampleData.general.phone)
+  const [website, setWebsite] = useState(exampleData.general.website)
+  const [location, setLocation] = useState(exampleData.general.location)
+  const [skills, setSkills] = useState(exampleData.general.skills)
+  const [interests, setInterests] = useState(exampleData.general.interests)
+  const [education, setEducation] = useState(exampleData.education)
+  const [experience, setExperience] = useState(exampleData.experience)
+  const [projects, setProjects] = useState(exampleData.projects)
+
+  function userInfo() {
+    const details = []
+
+    if (email.length > 0) {
+      details.push(email)
+    }
+
+    if (phone.length > 0) {
+      details.push(phone)
+    }
+
+    if (location.length > 0) {
+      details.push(location)
+    }
+
+    if (website.length > 0) {
+      details.push(website)
+    }
+
+    return details.join(' ❖ ')
+  }
+
   return (
     <div role='main' className='contentContainer'>
       <ResumeForm />
-      <ResumePreview />
+      <ResumePreview
+        name={name}
+        userInfo={userInfo()}
+        skills={skills}
+        interests={interests}
+        education={education}
+        experience={experience}
+        projects={projects}
+      />
     </div>
   )
 }
--- src/components/ResumePreview.jsx
+++ src/components/ResumePreview.jsx
@@ -1,7 +1,106 @@
-function ResumePreview() {
+function ResumePreview({
+  name,
+  userInfo,
+  skills,
+  interests,
+  education,
+  experience,
+  projects,
+}) {
   return (
     <div className="previewContainer">
-      <h3>Hello, World!</h3>
+      <div className="previewPage eb-garamond-normal">
+        {(name.length > 0 || email.length > 0 || phone.length > 0 || location.length > 0 || website.length > 0) && (
+          <section className="generalInfoPreview">
+            <h2 className="previewName eb-garamond-bold">{name}</h2>
+            <p className="previewUserInfo">{userInfo}</p>
+          </section>
+        )}
+
+        {experience.length > 0 && (
+          <section className="experiencePreview">
+            <h3 className="previewSectionHeader eb-garamond-bold">WORK EXPERIENCE</h3>
+            <ul>
+              {experience.map(exp => (
+                <li className="previewListItem" key={exp.key}>
+                  <div className="previewFlexContainer">
+                    <h3 className="previewListHeader eb-garamond-bold">{exp.employer}</h3>
+                    <h3 className="previewListHeader eb-garamond-bold">{exp.startDate} - {exp.endDate}</h3>
+                  </div>
+                  <div className="previewFlexContainer">
+                    <span className="previewListSubheader eb-garamond-italic">{exp.title}</span>
+                    <span className="previewListSubheader eb-garamond-italic">{exp.location}</span>
+                  </div>
+                  <ul className="bulletedList">
+                    {exp.descriptions.map(desc => (
+                      <li key={desc.key}>{desc.text}</li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        {projects.length > 0 && (
+          <section className="projectsPreview">
+            <h3 className="previewSectionHeader eb-garamond-bold">PERSONAL PROJECTS</h3>
+            <ul>
+              {projects.map(proj => (
+                <li className="previewListItem" key={proj.key}>
+                  <div className="projectFlexContainer">
+                    <h3 className="previewListHeader eb-garamond-bold">{proj.name}</h3>
+                    {/* &nbsp; is a non-breaking space character */}
+                    <span className="previewListSubheader eb-garamond-italic">&nbsp;| {proj.skills.join(', ')}</span>
+                    <a href={proj.livePreview} className="previewListLink rightJustify eb-garamond-bold-italic">Live Depoloyment</a>
+                    <span>&nbsp;|&nbsp;</span>
+                    <a href={proj.githubRepo} className="previewListLink eb-garamond-bold-italic">GitHub Link</a>
+                  </div>
+                  <ul className="bulletedList">
+                    {proj.descriptions.map(desc => (
+                      <li key={desc.key}>{desc.text}</li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        {education.length > 0 && (
+          <section className="educationPreview">
+            <h3 className="previewSectionHeader eb-garamond-bold">EDUCATION</h3>
+            <ul>
+              {education.map(edu => (
+                <li className="previewListItem" key={edu.key}>
+                  <div className="previewFlexContainer">
+                    <h3 className="previewListHeader eb-garamond-bold">{edu.school}</h3>
+                    <h3 className="previewListHeader eb-garamond-bold">{edu.completionDate}</h3>
+                  </div>
+                  <div className="previewFlexContainer">
+                    <span className="previewListSubheader eb-garamond-italic">{edu.degree}</span>
+                    <span className="previewListSubheader eb-garamond-italic">{edu.location}</span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        {skills.length > 0 || interests.length > 0 && (
+          <section className="skillsInterestPreview">
+            <h3 className="previewSectionHeader eb-garamond-bold">SKILLS & INTERESTS</h3>
+            <ul className="bulletedList">
+              <li className="previewSkills">
+                  <span className="eb-garamond-bold">Skills: </span>{skills.join(', ')}
+              </li>
+              <li className="previewInterests">
+              <span className="eb-garamond-bold">Interests: </span>{interests.join(', ')}
+              </li>
+            </ul>
+          </section>
+        )}
+      </div>
     </div>
   )
 }
